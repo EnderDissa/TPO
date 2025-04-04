f63dc48b6d85dd7321e425b1069b2acedbc403a1
Maksim Markov <148797503+MarkovMaksim2@users.noreply.github.com>
2025-04-04 18:56:10 +0300

    added drink state

--- lab1/third_task/app/src/main/kotlin/third_task/utils/objects/Drink.kt
+++ lab1/third_task/app/src/main/kotlin/third_task/utils/objects/Drink.kt
@@ -1,5 +1,12 @@
 package third_task.utils.objects
 
 class Drink(val name: String) {
+    val empty = false
+
+    fun isEmpty() = empty
+
+    fun drink() {
+        empty = true
+    } 
     override fun toString() = name
 }
