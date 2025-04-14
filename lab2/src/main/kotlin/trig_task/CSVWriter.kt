f28cf9485f38a2928dcb54a41fca3f4cf91f7109
Maksim Markov <148797503+MarkovMaksim2@users.noreply.github.com>
2025-04-14 21:41:13 +0300

    Added csv writer

---
+++ lab2/src/main/kotlin/trig_task/CSVWriter.kt
@@ -0,0 +1,27 @@
+package trig_task
+
+import java.io.FileWriter
+
+class CSVWriter(
+    private val writeDir: String,
+    private val delimiter: Char,
+    private val lineEnd: Char
+) {
+    fun writeData(
+        fileName: String,
+        startValue: Double,
+        endValue: Double,
+        numberOfRows: Int
+    ) {
+        if (fileName.isEmpty()) return
+        val fileWriter = FileWriter(writeDir + fileName)
+
+        val step = (endValue - startValue) / numberOfRows
+        fileWriter.write("argument" + delimiter + "result" + lineEnd)
+        for (i in 1..numberOfRows) {
+            fileWriter.write("" + (startValue + step * i) + delimiter + Equation.getResult(startValue + step * i) + lineEnd)
+        }
+
+        fileWriter.close()
+    }
+}
