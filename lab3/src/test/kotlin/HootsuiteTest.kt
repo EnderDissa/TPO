d3b468422a96b6dd99570e428487a850dcd20a71
EnderDIssa <54418849+EnderDIssa@users.noreply.github.com>
2025-06-03 19:09:15 +0300

    added two more tests

--- lab3/src/test/kotlin/HootsuiteTest.kt
+++ lab3/src/test/kotlin/HootsuiteTest.kt
@@ -31,4 +31,42 @@ class HootsuiteTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2))
         assertTrue(driver.pageSource?.contains("Pick the plan that's right for you")!!)
     }
+
+    @Test
+    fun requestDemoFormValidation() {
+        driver.get("https://www.hootsuite.com/request-demo")
+
+        val firstNameField = driver.findElement(By.xpath("//*[@id='FirstName']"))
+        val lastNameField = driver.findElement(By.xpath("//*[@id='LastName']"))
+        val emailField = driver.findElement(By.xpath("//*[@id='Email']"))
+        assertTrue(firstNameField.isDisplayed && firstNameField.isEnabled)
+        assertTrue(lastNameField.isDisplayed && lastNameField.isEnabled)
+        assertTrue(emailField.isDisplayed && emailField.isEnabled)
+
+        val countrySelect = driver.findElement(By.xpath("//*[@id='Country']"))
+        val helpSelect = driver.findElement(By.xpath("//*[@id='How_can_we_help_you_CP__c']"))
+        assertTrue(countrySelect.isDisplayed && countrySelect.isEnabled)
+        assertTrue(helpSelect.isDisplayed && helpSelect.isEnabled)
+
+        val submitButton = driver.findElement(By.xpath("//*[@id='marketo-17158']/div[31]/span/button"))
+
+        val isDisabled = !submitButton.isEnabled || submitButton.getAttribute("disabled") != null
+        assertTrue(isDisabled)
+    }
+    @Test
+    fun mobileMenuButtonTogglesDataTestId() {
+        driver.get("https://www.hootsuite.com")
+
+        val navElement = driver.findElement(By.xpath("//*[@id='site-header']/div[2]/nav"))
+
+        val initialTestId = navElement.getAttribute("data-testid")
+        assertEquals("mobileNav-close", initialTestId)
+
+        val menuButton = driver.findElement(By.xpath("//button[@data-testid='mobileNav-toggle']"))
+        menuButton.click()
+
+        val toggledTestId = navElement.getAttribute("data-testid")
+
+        assertEquals("mobileNav-open", toggledTestId)
+    }
 }
